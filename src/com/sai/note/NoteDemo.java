5d3376e1baf0b082c3e6db5854744ec37b5ed48f
sai <344319484@qq.com>
2024-06-03 22:09:54 +0800

    Changes

---
+++ src/com/sai/note/NoteDemo.java
@@ -0,0 +1,21 @@
+package com.sai.note;
+
+/**
+     目标：学会使用注释
+     这个程序就是讲解注释的
+ */
+public class NoteDemo {
+    public static void main(String[] args) {
+        //以下时一个打印语句，时往控制台输出内容的
+        System.out.println("我开始学习Java程序，好哈皮~~");
+
+        /*
+            窗前明月光，
+            凝视地上霜。
+            举头望明月，
+            低头思故乡。
+         */
+        System.out.println("播仔");
+        System.out.printf("dlei");
+    }
+}
